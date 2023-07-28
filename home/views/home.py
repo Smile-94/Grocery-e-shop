c819899a2e7a1399c7d70b4ab0551ec2bcbe51f8
Sazzad Hossen <mshossen75@gmail.com>
2023-07-29 01:19:27 +0600

    added recent product in landing page

--- home/views/home.py
+++ home/views/home.py
@@ -4,6 +4,9 @@ from django.shortcuts import redirect
 # Generc Classes
 from django.views.generic import TemplateView
 
+# Import Models
+from products.models import Product
+
 
 
 # Create your views here.
@@ -13,5 +16,6 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home" 
+        context["recent_product"] = Product.objects.filter(is_active=True).order_by('-id')[:10] 
         
         return context
