bbde18983158f2f0e6fe1b7c41218f74f15dbb83
Sazzad Hossen <mshossen75@gmail.com>
2023-07-29 17:50:42 +0600

    product page, product details page home page

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,8 @@ app_name='home'
 from home.views import home
 
 urlpatterns = [
-    path("", home.Home.as_view(), name="authority"),
+    path("", home.Home.as_view(), name="home"),
+    path("all-product", home.AllProductView.as_view(), name="all_products"),
+    path("product-details/<int:pk>/", home.ProductsDetailsView.as_view(), name="product_details"),
     
 ]
--- home/views/home.py
+++ home/views/home.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect
 
 # Generc Classes
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 # Import Models
 from products.models import Product
@@ -16,6 +18,33 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home" 
-        context["recent_product"] = Product.objects.filter(is_active=True).order_by('-id')[:10] 
+        context["recent_product"] = Product.objects.filter(is_active=True).order_by('-id')[:8] 
+        context["featured_product"] = Product.objects.filter(is_active=True, product_type='featured').order_by('-id')[:10] 
         
         return context
+
+class AllProductView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'home/all_products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "All Products" 
+        context["products"] = self.queryset
+        return context
+
+class ProductsDetailsView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'home/products_details.html'
+
+    def get_context_data(self, **kwargs):
+        product_pk = self.kwargs['pk']
+        product = Product.objects.get(id=product_pk)
+        sub_category = product.sub_catagory_1
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Product Details"
+        context["related_products"] = Product.objects.filter(sub_catagory_1=sub_category)[:4]
+        return context
+
+    
